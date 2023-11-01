3ecfe38d87e167cd6c1be455867a90585544a408
Mohd faizan Hussain <faizanhussain78612@gmail.com>
2023-11-01 15:36:25 +0530

    Complted Project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { useTheme } from '../context/ThemeContext'
 const Footer = () => {
     const {isDarkMode}=useTheme();
   return (
-<footer class={`${isDarkMode?'bg-cyan-900':'bg-emerald-300'} w-full p-4 mt-32`}>
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">Quote™</a>. All Rights Reserved.
+<footer className={`${isDarkMode?'bg-cyan-900':'bg-emerald-300'} w-full p-2 mt-28`}>
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Quote™</a>. All Rights Reserved.
     </span>
     </div>
 </footer>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTheme } from '../context/ThemeContext'
+import axios from 'axios'
+
+
 
 const Hero = () => {
-  const{isDarkMode}=useTheme()
+  const { isDarkMode } = useTheme()
+  const [quote, setQuote] = useState([])
+
+  
+
+  const options = {
+    method: 'GET',
+    url: 'https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes',
+    headers: {
+      'X-RapidAPI-Key':import.meta.env.VITE_RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
+    }
+  };
+
+
+  const getQuote = async () => {
+    try {
+      const response = await axios.request(options);
+      setQuote(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(()=>{
+    getQuote()
+  },[])
+
   return (
     <div>
-             <div className={`  w-4/5 max-w-sm  border ${isDarkMode?'bg-cyan-900 text-gray-100':'bg-emerald-300 text-gray-900'} rounded-lg shadow  m-auto py-14 p-6 mt-40`}>
-               
-               <div className="flex flex-col items-center pb-10">
-                 <h5 className={`mb-1 text-xl font-medium `}>
-                   Bonnie Green
-                 </h5>
-                 <span className={`text-sm`}>
-                   Visual Designer
-                 </span>
-                 <div className="flex mt-4 space-x-3 md:mt-6">
-                 <button type="button" class={` bg-gradient-to-r ${isDarkMode?'from-cyan-400 via-cyan-500 to-cyan-600 ':'from-lime-200 via-lime-400 to-lime-500 '}  hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2`}>Get Quotes</button>
-                 </div>
-               </div>
-             </div>
-           </div>
+      <div className={`w-4/5 max-w-sm  border ${isDarkMode ? 'bg-cyan-900 text-gray-100' : 'bg-emerald-300 text-gray-900'} rounded-lg shadow  m-auto py-14 p-6 mt-40`}>
+        {quote.map((quote) => (
+          <div className="flex flex-col items-center pb-10">
+            <h5 className={`mb-1 text-lg font-medium `}>
+              {quote.quote}
+            </h5>
+            <span className={`text-sm`}>
+              ~{quote.author}
+            </span>
+            <div className="flex mt-4 space-x-3 md:mt-6">
+              <button onClick={() => getQuote()} type="button" class={` bg-gradient-to-r ${isDarkMode ? 'from-cyan-400 via-cyan-500 to-cyan-600 ' : 'from-lime-200 via-lime-400 to-lime-500 '}  hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2`}>Get Quotes</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
 export default Hero
-
-
-        
